ef9837090520d3756f0deea872a1367cd7d74a49
dancielos <iam@dancielos.com>
2023-10-30 22:09:10 -0600

    finished the nextjs 14 tutorial

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -3,7 +3,8 @@
 import { sql } from '@vercel/postgres';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
-import { custom, z } from 'zod';
+import { z } from 'zod';
+import { signIn } from '@/auth';
 
 const InvoiceSchema = z.object({
 	id: z.string(),
@@ -116,4 +117,18 @@ export async function deleteInvoice(formData: FormData) {
 		return { message: 'Database error: Failed to delete an invoice.' };
 	}
 	revalidatePath('/dashboard/invoices');
+}
+
+export async function authenticate(
+	prevState: string | undefined,
+	formData: FormData
+) {
+	try {
+		await signIn('credentials', Object.fromEntries(formData));
+	} catch (error) {
+		if ((error as Error).message.includes('CredentialsSignin')) {
+			return 'CredentialsSignin';
+		}
+		throw error;
+	}
 }
